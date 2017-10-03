f124d84f346d1313f554c808492c2bdae83a3cc4
Matt Braden <matt.braden73@gmail.com>
2017-10-03 16:57:58 -0400

    I added bounds to the camera

--- Assets/scripts/player controls/PlayerMovement.cs
+++ Assets/scripts/player controls/PlayerMovement.cs
@@ -16,6 +16,12 @@ public class PlayerMovement : MonoBehaviour {
 	void FixedUpdate () {
         yPos += speed* Input.GetAxis("Vertical");
         xPos += speed * Input.GetAxis("Horizontal");
+        Vector3 boundsMax = Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.pixelRect.xMax, Camera.main.pixelRect.yMax, 0));
+        Vector3 boundsMin = Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.pixelRect.xMin, Camera.main.pixelRect.yMin, 0));
+        if (yPos > boundsMax.y) yPos = boundsMax.y;
+        if (xPos > boundsMax.x) xPos = boundsMax.x;
+        if (yPos < boundsMin.y) yPos = boundsMin.y;
+        if (xPos < boundsMin.x) xPos = boundsMin.x;
         gameObject.transform.position = new Vector2(xPos, yPos);
     }
 }
