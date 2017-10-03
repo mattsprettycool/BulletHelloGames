0188c28cd56686488a1d605f4eb4d942c8b2da0a
Matt Braden <matt.braden73@gmail.com>
2017-10-03 16:14:55 -0400

    changed stuff

---
+++ Assets/scripts/player attacks/PlayerShooting.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour {
+    [SerializeField]
+    GameObject bullet;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxis("Fire1") != 0) Instantiate(bullet, gameObject.transform);
+	}
+}
---
+++ Assets/scripts/player controls/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    float yPos, xPos;
+    [SerializeField]
+    float speed;
+	// Use this for initialization
+	void Start () {
+        yPos = gameObject.transform.position.y;
+        xPos = gameObject.transform.position.x;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        yPos += speed* Input.GetAxis("Vertical");
+        xPos += speed * Input.GetAxis("Horizontal");
+        gameObject.transform.position = new Vector2(xPos, yPos);
+    }
+}
