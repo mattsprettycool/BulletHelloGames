c0f20982c8c4a2b0b19718c268d04455b490a2c4
Lucas Beutler <LucasBeutl@users.noreply.github.com>
2017-10-10 15:03:11 -0400

    basic enemy AI

---
+++ Assets/scripts/Enemy AI/Turret/EnemyShooting.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShooting : MonoBehaviour {
+
+	public GameObject bulletPrefrab;
+
+	public Vector3 bulletOffset = new Vector3 (0, 0.5f, 0); 
+
+	public float fireDelay = 0.5f;
+	float cooldownTimer = 0;
+
+	// Update is called once per frame
+	void Update () {
+		cooldownTimer -= Time.deltaTime;
+
+		if (cooldownTimer <= 0) {
+			//Shoot!
+			Debug.Log ("PEW!");
+			cooldownTimer = fireDelay;
+
+			Vector3 offset = transform.rotation * bulletOffset; 
+
+			GameObject bulletGo = (GameObject)Instantiate (bulletPrefrab, transform.position + offset, transform.rotation); 
+			bulletGo.layer = gameObject.layer; 
+		}
+	}
+}
+
---
+++ Assets/scripts/Enemy AI/Turret/facesPlayer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class facesPlayer : MonoBehaviour {
+
+	public float rotSpeed = 180f;
+
+	Transform player;
+	
+	// Update is called once per frame
+	void Update () {
+		if (player == null) {
+		 	//find the players ship!
+			GameObject go = GameObject.FindWithTag("Player");
+			if (go != null) {
+				player = go.transform;
+			}
+		}
+		//at this poit we have found a player or the player dose not exist right now 
+		if (player == null)
+			return; // try again next frame!
+
+		//Here whe know for sure we have a player. Turn to face it!
+
+		Vector3 dir = player.position - transform.position; 
+		dir.Normalize ();
+
+		float zAngle = Mathf.Atan2 (dir.y, dir.x) * Mathf.Rad2Deg - 90;
+
+		Quaternion desiredRot  = Quaternion.Euler (0, 0, zAngle); 
+
+		transform.rotation = Quaternion.RotateTowards (transform.rotation, desiredRot, rotSpeed * Time.deltaTime);
+
+	}
+}
