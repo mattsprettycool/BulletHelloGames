b442bfc3c46f57640173a2945e212b8d7305fb54
Matt Braden <matt.braden73@gmail.com>
2017-10-25 13:43:18 -0400

    I started work on enemy bullets. You can check it out in the enemy object.

---
+++ Assets/scripts/Enemy AI/path enemy/EnemyGun.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGun : MonoBehaviour {
+    bool continueRunning = true;
+    enum BulletType
+    {
+        BasicForwardShot, SlowLargeShot
+    }
+    [SerializeField]
+    BulletType attackPattern = new BulletType();
+    [SerializeField]
+    GameObject basicForwardShot, slowLargeShot;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (continueRunning)
+        {
+            if (attackPattern == BulletType.BasicForwardShot)
+            {
+                StartCoroutine((WaitForTime(.25f)));
+                Shoot(basicForwardShot);
+            }else if(attackPattern == BulletType.SlowLargeShot)
+            {
+                StartCoroutine((WaitForTime(.5f)));
+                Shoot(slowLargeShot);
+            }
+        }
+	}
+    void Shoot(GameObject g)
+    {
+        Instantiate(g,gameObject.transform.position,gameObject.transform.rotation);
+    }
+    IEnumerator WaitForTime(float timeToWait)
+    {
+        continueRunning = false;
+        yield return new WaitForSeconds(timeToWait);
+        continueRunning = true;
+    }
+}
---
+++ Assets/scripts/Enemy Bullet Types/BasicForwardShot.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Author: Matt Braden
+public class BasicForwardShot : MonoBehaviour {
+    float yVal;
+    [SerializeField]
+    float speed;
+    [SerializeField]
+    float damage;
+    // Use this for initialization
+    void Start () {
+        yVal = gameObject.transform.position.y;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        //increases the yvalue and then kills the bullet if it's at the top of the screen
+        yVal -= speed;
+        gameObject.transform.position = new Vector2(gameObject.transform.position.x, yVal);
+        if (yVal < Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0)).y) Destroy(gameObject);
+    }
+}
