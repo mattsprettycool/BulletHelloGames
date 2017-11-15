6a21b3b455555dce767ad6ac4ab7361dc2197b29
Matt Braden <matt.braden73@gmail.com>
2017-11-15 12:37:00 -0500

    I made the game a lot larger and the player a lot smaller. play on fullscreen to get the full effect.

--- Assets/scripts/pixelPerfectStuff/PixelPerfectCam.cs
+++ Assets/scripts/pixelPerfectStuff/PixelPerfectCam.cs
@@ -4,6 +4,6 @@ using UnityEngine;
 
 public class PixelPerfectCam : MonoBehaviour {
 	void Awake () {
-        Camera.main.orthographicSize = Screen.height / 2;
+        Camera.main.orthographicSize = 318;
 	}
 }
