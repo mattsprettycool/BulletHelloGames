e549ae72a457ff6c1539bd4ce4ecd587eab28967
Matt Braden <matt.braden73@gmail.com>
2017-11-21 15:31:22 -0500

    I started work on the squid boss

---
+++ Assets/scripts/squidboss/Stage1Tentacles.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stage1Tentacles : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        gameObject.transform.localScale = new Vector3(Camera.main.pixelWidth, gameObject.transform.localScale.y, gameObject.transform.localScale.z);
+        gameObject.transform.position = new Vector3(- 1 * (Camera.main.pixelWidth / 2), gameObject.transform.position.y, gameObject.transform.position.z);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
