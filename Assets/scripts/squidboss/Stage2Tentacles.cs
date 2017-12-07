13bd791b3ef0ba137b0f641ada5c4e93e7842d4f
Matt Braden <matt.braden73@gmail.com>
2017-12-07 14:16:34 -0500

    ...

--- Assets/scripts/squidboss/Stage2Tentacles.cs
+++ Assets/scripts/squidboss/Stage2Tentacles.cs
@@ -7,6 +7,11 @@ public class Stage2Tentacles : MonoBehaviour {
     [SerializeField, Range(1,100)]
     int baseSpeed = 1;
     float speed = 0;
+    int debugTimesAttacked = 0;
+    [SerializeField, Range(1, 100)]
+    int timeUntilAttack = 1;
+    float attackTimer = 0;
+    bool firedOnce = false;
     // Use this for initialization
     void Start () {
         playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
@@ -14,17 +19,42 @@ public class Stage2Tentacles : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if(transform.position.y > playerTransform.position.y && transform.position.y-baseSpeed > playerTransform.position.y)
+        if(transform.position.y > playerTransform.position.y && transform.position.y-baseSpeed > playerTransform.position.y && !firedOnce)
         {
             if (!(speed <= -baseSpeed))
                 speed -= .1f;
-        }else if(transform.position.y < playerTransform.position.y && transform.position.y + baseSpeed < playerTransform.position.y)
+        }else if(transform.position.y < playerTransform.position.y && transform.position.y + baseSpeed < playerTransform.position.y && !firedOnce)
         {
             if (!(speed >= baseSpeed))
                 speed += .1f;
         }
-        if (transform.position.y <= baseSpeed + playerTransform.position.y && transform.position.y >= playerTransform.position.y - baseSpeed)
+        if (transform.position.y <= baseSpeed + playerTransform.position.y && transform.position.y >= playerTransform.position.y - baseSpeed && !firedOnce)
             speed /= 1.25f;
-        transform.Translate(0, speed, 0);
+        if (!firedOnce)
+        {
+            transform.Translate(0, speed, 0);
+        }
+        else
+            speed = 0;
+            
+        if (attackTimer >= timeUntilAttack && !firedOnce)
+        {
+            firedOnce = true;
+            debugTimesAttacked++;
+            Debug.Log("attacked for the " + debugTimesAttacked + " time.");
+            DoAttack();
+        }
+        else
+            attackTimer += Time.deltaTime;
+    }
+    void DoAttack()
+    {
+        float i = 0;
+        while(i<=10)
+        {
+            i += Time.deltaTime;
+        }
+        firedOnce = false;
+        attackTimer = 0;
     }
 }
